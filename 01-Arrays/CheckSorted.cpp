5f558f5a8cfdb0b2727b061db8cd08e74b1d4562
waghuldeketaki-lgtm <waghuldeketaki@gmail.com>
2026-07-26 16:45:03 +0530

    Add check sorted array solution

---
+++ 01-Arrays/CheckSorted.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bool isSorted = true;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            isSorted = false;
+            break;
+        }
+    }
+
+    if (isSorted)
+        cout << "Array is sorted." << endl;
+    else
+        cout << "Array is not sorted." << endl;
+
+    return 0;
+}
